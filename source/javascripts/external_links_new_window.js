d47e0e52428e2ceb5c5ccc3f9ec5d3b83f74c7f4
Winston Kotzan <wak@wakproductions.com>
2016-07-03 10:56:54 -0400

    Clean out old source folder

--- source/javascripts/external_links_new_window.js
+++
@@ -1,6 +0,0 @@
-// This will make off-site links open in a new window
-$(document.links).filter(function() {
-    //return this.hostname != window.location.hostname;
-    return this.hostname != 'localhost';
-    alert('link has been clicked')
-}).attr('target', '_blank');
